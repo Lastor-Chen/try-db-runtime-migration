b2d24c42b7370fe2de845be2940c6a888bbfd665
Lastor <lastorchen@ikv.tw>
2025-01-09 10:40:23 +0800

    feat: cli 加入新 db sync

--- src/umzug/cli.ts
+++ src/umzug/cli.ts
@@ -2,21 +2,41 @@ import path from 'node:path'
 import { Sequelize } from 'sequelize'
 import { Umzug, SequelizeStorage } from 'umzug'
 
+import { createModels } from './models/index.js'
 import { loadMigrations } from './migrations/index.js'
 
+// init sequelize
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: path.join(process.cwd(), './database.db'),
   logging: false,
 })
 
-const migrations = loadMigrations(path.join(import.meta.dirname, './migrations'))
+const models = createModels(sequelize)
 
+// init migrator
+const migrations = loadMigrations(path.join(import.meta.dirname, './migrations'))
+const storage = new SequelizeStorage({ sequelize })
 const umzug = new Umzug({
   migrations: migrations,
   context: sequelize.getQueryInterface(),
-  storage: new SequelizeStorage({ sequelize }),
+  storage,
   logger: console,
 })
 
-umzug.runAsCLI()
+// check db migrations status
+const queryInterface = sequelize.getQueryInterface()
+const tables = await queryInterface.showAllTables()
+const isNewDb = tables.length === 0
+if (isNewDb) {
+  console.log('[CLI] New db detected, sync tables schema\n')
+  await sequelize.sync()
+
+  const pendingMigrations = await umzug.pending()
+  await Promise.all(
+    pendingMigrations.map(({ name }) => storage.logMigration({ name }))
+  )
+}
+
+// start cli
+await umzug.runAsCLI()
